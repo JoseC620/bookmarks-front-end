ac9a166d86511ec06104ae3fe8023f43fcfb1f1f
JoseC620 <Jcepeda6201@gmail.com>
2023-05-01 20:24:23 -0400

    second

--- src/Components/ReviewForm.js
+++ src/Components/ReviewForm.js
@@ -1,16 +1,93 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export default function ReviewForm() {
+export default function ReviewForm(props) {
 const { id } = useParams();
+const {reviewDetails} = props
+const [reveiw, setReview] = useState({
+    reviewer: "",
+    title: "",
+    content: "",
+    rating: "",
+    bookmark_id: id,
+})
 
+const handleTextChange = (event) => {
+    setReview({...reviews, [event.target.id]: event.target.value})
+}
+
+useEffect(() => {
+    if(reviewDetails){
+        setReview(reviewDetails)
+    }
+}, [reviewDetails])
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    props.handleSubmit(review, id)
+    if(reviewDetails) {
+        props.toggleView()
+    }
+    setReview({
+        reviewer: "",
+        title: "",
+        content: "",
+        rating: "",
+        bookmark_id: id,
+    })
+}
 
 
 
 
     return (
         <div>
+            <div className="Edit">
+      {props.children}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="reviewer">Name:</label>
+        <input
+          id="reviewer"
+          value={review.reviewer}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Your name"
+          required
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          required
+          value={review.title}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="rating">Rating:</label>
+        <input
+          id="rating"
+          type="number"
+          name="rating"
+          min="0"
+          max="5"
+          step="1"
+          value={review.rating}
+          onChange={handleTextChange}
+        />
+        <label htmlFor="content">Review:</label>
+        <textarea
+          id="content"
+          type="text"
+          name="content"
+          value={review.content}
+          placeholder="What do you think..."
+          onChange={handleTextChange}
+        />
 
+        <br />
+
+        <input type="submit" />
+      </form>
+    </div>
         </div>
     )
 
